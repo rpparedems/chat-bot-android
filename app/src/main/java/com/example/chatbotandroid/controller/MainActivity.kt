e222128365e48604d56960c1c4fa020798e08b40
Ronny Paredes <ronnyparedes@DESKTOP-PMVKPCB.corp.microsoft.com>
2021-06-03 16:00:55 -0400

    Updated views and added websocket  client to communicate with server

--- app/src/main/java/com/example/chatbotandroid/controller/MainActivity.kt
+++ app/src/main/java/com/example/chatbotandroid/controller/MainActivity.kt
@@ -3,23 +3,111 @@ package com.example.chatbotandroid.controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.chatbotandroid.R
-import android.content.Intent
-import android.net.Uri
 import androidx.recyclerview.widget.LinearLayoutManager
-
+import com.example.chatbotandroid.model.ChatAdapter
+import com.example.chatbotandroid.model.Message
+import com.example.chatbotandroid.utils.Constants
+import com.example.chatbotandroid.utils.Time
+import org.java_websocket.client.WebSocketClient
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
+import org.java_websocket.handshake.ServerHandshake
+import java.lang.Exception
+import java.net.URI
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: ChatAdapter
+    private lateinit var webSocketClient: WebSocketClient
+    private val webSocketUri: URI? = URI("ws://10.0.2.2:8080/chat")
+
+    //Life-cycle methods
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        registerClickEvents()
+        recyclerView()
+        registerOnClickListeners()
     }
-    private fun registerClickEvents(){
-        btn_send.setOnClickListener {
-            //sendMessage()
-            println("Pressing send button")
+
+    override fun onResume() {
+        super.onResume()
+        initWebSocket()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        //Close web socket if app is on background to avoid unnecessary active connection
+        webSocketClient.close()
+    }
+
+    //Websocket methods
+    private fun initWebSocket() {
+        createWebSocketClient(webSocketUri)
+        webSocketClient.connect()
+    }
+
+    private fun createWebSocketClient(coinbaseUri: URI?) {
+        println("createws")
+        webSocketClient = object : WebSocketClient(coinbaseUri) {
+            override fun onOpen(handshakedata: ServerHandshake?) {
+                println("onOpen")
+
+            }
+            override fun onMessage(message: String?) {
+                if (message != null) {
+                    addBotResponse(message)
+                }
+            }
+            override fun onClose(code: Int, reason: String?, remote: Boolean) {
+                println("onClose")
+                println(reason)
+            }
+            override fun onError(ex: Exception?) {
+                println("onError")
+                println(ex)
+            }
         }
     }
+
+    //Helper methods
+    private fun registerOnClickListeners(){
+        //Send message from client to server
+        send_button.setOnClickListener {
+            sendMessage()
+        }
+    }
+
+    private fun recyclerView() {
+        adapter = ChatAdapter()
+        recycler_view_chat.adapter = adapter
+        recycler_view_chat.layoutManager = LinearLayoutManager(applicationContext)
+
+    }
+
+    private fun sendMessage() {
+        val message = input_message.text.toString()
+
+        if (message.isNotEmpty()) {
+            //Clean message input
+            input_message.setText("")
+            //Pass message to adapter to add it to chat view
+            adapter.insertMessage(Message(message, Constants.SEND_TYPE, Time.timeStamp()))
+            recycler_view_chat.scrollToPosition(adapter.itemCount - 1)
+            webSocketClient.send(message)
+        }
+    }
+
+    private fun addBotResponse(message: String) {
+        GlobalScope.launch {
+            //Simulate network latency
+            delay(500)
+            withContext(Dispatchers.Main) {
+                //Inserts sender message
+                adapter.insertMessage(Message(message, Constants.RECEIVE_TYPE, Time.timeStamp()))
+
+                //Scroll to the position of the latest message
+                recycler_view_chat.scrollToPosition(adapter.itemCount - 1)
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/chatbotandroid/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.chatbotandroid.utils
+
+object Constants {
+    val SEND_TYPE = "SENDER"
+    val RECEIVE_TYPE = "RECEIVER"
+}
